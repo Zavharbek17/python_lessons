359d64df7c59e9821e831477f92574ebb587cef0
Zavharbek <63339853+Zavharbek17@users.noreply.github.com>
2025-06-26 22:38:28 +0500

    new files added
    new files added

---
+++ lesson_if_else.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu May 29 22:25:11 2025
+
+@author: Zavharbek
+"""
+avtolar = ['audi', 'bmw','volvo', 'kia', 'hyundai']
+for avto in avtolar:
+    if avto =='bmw':
+        print(avto.upper())
+    else:
+        print(avto.title())
+        
+        
