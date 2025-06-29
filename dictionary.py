85176660f8d71a57171e3b2b5ddb289c3610c0de
Zavharbek <63339853+Zavharbek17@users.noreply.github.com>
2025-06-29 23:15:16 +0500

    Create dictionary.py

---
+++ dictionary.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jun 27 09:17:22 2025
+
+@author: Zavharbek
+"""
+
+#Dictionary (Lug'at)
+
+car_0 ={'model':'ferrare', 'rang':'qizil'}
+print(car_0['model'])
+print(car_0['rang'])
